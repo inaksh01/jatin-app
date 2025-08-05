7e286612ce6fadbc3f5d1878d6d55ac3d3d1cfee
inaksh01 <nakshgaur009@gmail.com>
2025-08-05 21:56:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from io import BytesIO
 MODEL_PATH = "best.pt"
 if not os.path.exists(MODEL_PATH):
     st.info("📦 Downloading model from Google Drive...")
-    file_id = "18O0SvhxoP1hCWkRUPUKsDR1E2bCPLVMRT" #18O0SvhxoP1hCWkRUPUKsDR1E2bCPLVMR
+    file_id = "18O0SvhxoP1hCWkRUPUKsDR1E2bCPLVMRT" 
     url = f"https://drive.google.com/uc?id={file_id}"
     gdown.download(url, MODEL_PATH, quiet=False)
 
